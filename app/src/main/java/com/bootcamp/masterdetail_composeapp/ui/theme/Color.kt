2ebd03de949b16a204d40275854e1d7707215826
JavFuentes <javier.antonio8128@gmail.com>
2023-05-13 03:33:36 -0400

    Comenzando proyecto

---
+++ app/src/main/java/com/bootcamp/masterdetail_composeapp/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.bootcamp.masterdetail_composeapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val MoreLightBlueAndroid = Color(0xFFD7EFFE)
+val AndroidGreen = Color(0xFF00DE7A)
+val LightBlueAndroid = Color(0xFF4285F4)
+val BlueAndroid = Color(0xFF083042)
